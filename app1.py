64b5aa12caee032dd6103e51fa5cacc9814d7656
Bluesman66 <alex.paclin@gmail.com>
2019-12-31 01:27:15 +0500

    084 Prompting the User to Confirm Similarity Check

--- app1.py
+++ app1.py
@@ -10,9 +10,15 @@ def translate(word):
     else:
         match = get_close_matches(word, data.keys())
         if len(match) > 0:
-            return "did you mean \'{}\' instead?".format(match[0])
+            res = input("Did you mean \'{}\' instead? Enter Y if Yes or N if No.".format(match[0]))
+            if res == "Y":
+                return data[match[0]]
+            elif res == "N":
+                return "The word \'{}\' does not exist. Please double check it.".format(word)
+            else:
+                return "Sorry. We didn't understand your entry."    
         else:
-            return "The word {} does not exist. Please double check it."
+            return "The word \'{}\' does not exist. Please double check it.".format(word)
 
 word = input("Enter the world: ")
 
